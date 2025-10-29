8e243833a042527f3c512409d3ab754f45baa1e1
geisyv <72949547+geisyv@users.noreply.github.com>
2025-10-29 02:57:36 -0300

    segundo commit
    ajustes finais

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 export default function App() {
+
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+
+  const phoneNumber = "5511964371888";
+
+  const handleSend = () => {
+    const encodedMessage = encodeURIComponent(`Olá, meu nome é ${name}. ${message}`);
+    const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+    window.open(whatsappURL, "_blank");
+
+    setName("");
+    setMessage("");
+  };
+
+  const [showArrow, setShowArrow] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setShowArrow(window.scrollY > 300);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+
   return (
     <div className="App">
       <header className="retro-header">
@@ -33,7 +59,7 @@ export default function App() {
         </div>
       </header>
 
-      <section className="intro-section">
+      <section id="inicio" className="intro-section">
         <div className="intro-text">
           <h1>Olá, meu nome é Geisy</h1>
           <h2>Transformo ideias em <span className="highlight">realidade digital.</span></h2>
@@ -41,7 +67,6 @@ export default function App() {
             Sou desenvolvedora web com experiência em HTML, CSS, Javascript e React. Tenho paixão por criar interfaces modernas,
             funcionais e acessíveis, sempre buscando desafios que me ajudem a evoluir profissionalmente.
           </p>
-          <button className="contact-btn">Entre em contato</button>
         </div>
 
         <div className="intro-image">
@@ -50,8 +75,12 @@ export default function App() {
       </section>
 
       <section id="sobre" className="sobre-section">
+        <div class="header">
+          <img width="50" height="50" src="https://img.icons8.com/ios/50/about-me.png" alt="about-me" />
+          <h2>Sobre mim</h2>
+          <img width="50" height="50" src="https://img.icons8.com/ios/50/about-me.png" alt="about-me" />
+        </div>
         <div className="sobre-container">
-          <h2>Sobre Mim</h2>
           <p>
             Profissional em início de carreira, apaixonada por dados e tecnologia, com foco no desenvolvimento de habilidades em Salesforce e Ciência de Dados. Tenho conhecimentos iniciais em administração e desenvolvimento Salesforce e busco constantemente aprender mais para aplicar soluções que tragam valor estratégico.
           </p>
@@ -67,56 +96,90 @@ export default function App() {
         </div>
       </section>
 
-      <section class="featured-projects">
+      <section id="projetos" class="featured-projects">
         <div class="header">
-          <img width="50" height="50" src="https://img.icons8.com/ios/50/laptop--v1.png" alt="laptop--v1"/>
-          <h2>Featured Projects</h2>
-          <img width="50" height="50" src="https://img.icons8.com/ios/50/laptop--v1.png" alt="laptop--v1"/>
+          <img width="50" height="50" src="https://img.icons8.com/ios/50/laptop--v1.png" alt="laptop--v1" />
+          <h2>Meus projetos</h2>
+          <img width="50" height="50" src="https://img.icons8.com/ios/50/laptop--v1.png" alt="laptop--v1" />
         </div>
 
         <div class="projects-grid">
           <div class="project-card">
-            <img src="site-jeje.png" alt="Vintage Records"/>
-              <div class="tags">
-                <span class="tag yellow">Blog</span>
-                <span class="tag purple">Projeto Pessoal</span>
-              </div>
-              <h3>Site sobre o candomblé Jeje-Mahi</h3>
-              <p>Site que visa reunir informações sobre a história da nação Jeje-Mahi no Brasil.</p>
-              <a href="https://github.com/geisyv/site-sobre-jeje" target="_blank" class="btn">Ver Projeto →</a>
+            <img src="site-jeje.png" alt="Vintage Records" />
+            <div class="tags">
+              <span class="tag yellow">Blog</span>
+              <span class="tag purple">Projeto Pessoal</span>
+            </div>
+            <h3>Site sobre o candomblé Jeje-Mahi</h3>
+            <p>Site que visa reunir informações sobre a história da nação Jeje-Mahi no Brasil.</p>
+            <a href="https://github.com/geisyv/site-sobre-jeje" target="_blank" class="btn">Ver Projeto →</a>
           </div>
 
           <div class="project-card">
-            <img src="site1.jpg" alt="Growth Case Study"/>
-              <div class="tags">
-                <span class="tag yellow">Growth Design</span>
-                <span class="tag purple">Mobile</span>
-              </div>
-              <h3>66M Users in a Year</h3>
-              <p>Como ajudamos uma startup a escalar de 0 a 66 milhões de usuários em 1 ano.</p>
-              <a href="https://github.com/seuusuario/projeto2" target="_blank" class="btn">Ver Projeto →</a>
+            <img src="site1.jpg" alt="Growth Case Study" />
+            <div class="tags">
+              <span class="tag yellow">Growth Design</span>
+              <span class="tag purple">Mobile</span>
+            </div>
+            <h3>66M Users in a Year</h3>
+            <p>Como ajudamos uma startup a escalar de 0 a 66 milhões de usuários em 1 ano.</p>
+            <a href="https://github.com/seuusuario/projeto2" target="_blank" class="btn">Ver Projeto →</a>
           </div>
 
           <div class="project-card">
-            <img src="site2.png" alt="RTL Website"/>
-              <div class="tags">
-                <span class="tag blue">Localization</span>
-                <span class="tag green">Website</span>
-              </div>
-              <h3>Website RTL</h3>
-              <p>Site multilíngue com suporte para idiomas da direita para a esquerda.</p>
-              <a href="https://github.com/seuusuario/projeto3" target="_blank" class="btn">Ver Projeto →</a>
+            <img src="site2.png" alt="RTL Website" />
+            <div class="tags">
+              <span class="tag blue">Localization</span>
+              <span class="tag green">Website</span>
+            </div>
+            <h3>Website RTL</h3>
+            <p>Site multilíngue com suporte para idiomas da direita para a esquerda.</p>
+            <a href="https://github.com/seuusuario/projeto3" target="_blank" class="btn">Ver Projeto →</a>
           </div>
         </div>
       </section>
 
+      <main id="contato" className="contact-section">
 
+        <div className="header">
+          <img width="50" height="50" src="https://img.icons8.com/ios/50/connected-people.png" alt="connected-people" />
+          <h2>Entre em contato</h2>
+          <img width="50" height="50" src="https://img.icons8.com/ios/50/connected-people.png" alt="connected-people" />
+        </div>
 
+        <div className="contact-left">
+          <div className="social-item">
+            <img src="https://img.icons8.com/ios-filled/30/github.png" alt="GitHub" />
+            <a href="https://github.com/geisyv" target="_blank" rel="noopener noreferrer">GitHub</a>
+          </div>
 
+          <div className="social-item">
+            <img src="https://img.icons8.com/ios-filled/30/linkedin.png" alt="LinkedIn" />
+            <a href="https://www.linkedin.com/in/geisielly-vasques/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+          </div>
+        </div>
 
-      <main className="content">
-        <p>Bem-vindo ao meu portfólio retrô!</p>
+        <div className="contact-right">
+          <input
+            type="text"
+            placeholder="Seu nome"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <textarea
+            placeholder="Sua mensagem"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button onClick={handleSend}>Enviar</button>
+        </div>
       </main>
+
+
+      {showArrow && (
+        <a href="#inicio" className="scroll-to-top" title="Voltar ao topo">↑</a>
+      )}
+
     </div>
   );
 }
